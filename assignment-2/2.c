f24fdfe1b59882ce7c6148e5a1d62584aef0d971
Shekel-JF <129303182+Shekel-JF@users.noreply.github.com>
2023-03-30 19:14:56 +0200

    Add files via upload

---
+++ assignment-2/2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int i = 0;
+    int id = 0;
+    int stat;
+		
+	printf("Proces macierzysty\n");
+	printf("UID: %d\n", getuid());
+	printf("GID: %d\n", getgid());
+	printf("PID: %d\n", getpid());
+	printf("PPID: %d\n", getppid());
+	printf("PGID: %d\n", getpgid(0));
+
+    for (i = 0; i < 3; i = i + 1)
+    {
+        id = fork();
+        switch (id)
+        {
+            case -1:
+                perror("fork error");
+                exit(1);
+            case 0:                
+                execl ("./procesy.x","procesy",NULL);
+                perror("execl error");
+                _exit(2);
+                
+            default:
+                wait(NULL);            
+        }
+    }
+}
